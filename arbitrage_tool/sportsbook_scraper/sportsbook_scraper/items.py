040cb4b5fe397249c1027b63477c625bf953d3ea
Blair Bowen <blairbowen35@gmail.com>
2024-08-06 13:25:54 -0400

    working nice for 2out+spread

--- arbitrage_tool/sportsbook_scraper/sportsbook_scraper/items.py
+++ arbitrage_tool/sportsbook_scraper/sportsbook_scraper/items.py
@@ -4,9 +4,21 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader import ItemLoader
+from itemloaders.processors import TakeFirst, MapCompose
+from w3lib.html import remove_tags
 
 
 class SportsbookScraperItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    sideA = scrapy.Field(input_processor = MapCompose(remove_tags), output_processor = TakeFirst())
+    sideB = scrapy.Field(input_processor = MapCompose(remove_tags), output_processor = TakeFirst())
+    spread = scrapy.Field(input_processor = MapCompose(remove_tags), output_processor = TakeFirst())
+    spreadA = scrapy.Field(input_processor = MapCompose(remove_tags), output_processor = TakeFirst())
+    spreadB = scrapy.Field(input_processor = MapCompose(remove_tags), output_processor = TakeFirst())
+    total = scrapy.Field(input_processor = MapCompose(remove_tags), output_processor = TakeFirst())
+    over = scrapy.Field(input_processor = MapCompose(remove_tags), output_processor = TakeFirst())
+    under = scrapy.Field(input_processor = MapCompose(remove_tags), output_processor = TakeFirst())
+    moneyA = scrapy.Field(input_processor = MapCompose(remove_tags), output_processor = TakeFirst())
+    moneyB = scrapy.Field(input_processor = MapCompose(remove_tags), output_processor = TakeFirst())
+    moneyC = scrapy.Field(input_processor = MapCompose(remove_tags), output_processor = TakeFirst())
--- arbitrage_tool/sportsbook_scraper/sportsbook_scraper/spiders/sportsbookspider.py
+++ arbitrage_tool/sportsbook_scraper/sportsbook_scraper/spiders/sportsbookspider.py
@@ -1,14 +1,15 @@
 from typing import Iterable
 import scrapy
 from scrapy_playwright.page import PageMethod
+from sportsbook_scraper.items import SportsbookScraperItem
+from scrapy.loader import ItemLoader
 
 
 class SportsbookspiderSpider(scrapy.Spider):
     name = "sportsbookspider"
 
     def start_requests(self) -> Iterable[scrapy.Request]:
-        # url = 'file:///home/bowenbv/Code/arbitrage_tool/arbitrage_tool/sportsbook_scraper/test.html'
-        url = 'https://espnbet.com/sport/football/organization/united-states/competition/nfl'
+        url = 'https://espnbet.com/sport/rugby-league/organization/australia/competition/nrl'
         yield scrapy.Request(
             url=url,
             meta=dict(
@@ -25,96 +26,19 @@ class SportsbookspiderSpider(scrapy.Spider):
         containers = response.xpath('//div[@class="flex flex-col gap-3 rounded p-4 bg-card-primary-temp"]')
         
         for container in containers:
-            # # Extract all text within the current container and its descendants
-            # # all_text = container.xpath('.//text()').getall()
-            
-            # # # Clean up the extracted text
-            # # cleaned_text = [text.strip() for text in all_text if text.strip()]
-            
-            # # yield {'container_text': container.css('div.text-style-s-medium text-primary.text-primary'.replace(' ', '.'))}
+            loader = ItemLoader(item=SportsbookScraperItem(), selector=container)
 
-            # # buttons = 'button.relative rounded flex h-[48px] flex-1 flex-col items-center justify-center p-1.5 text-style-xs-medium button-bet-selector-deselected'.replace(' ', '.')
+            loader.add_xpath('sideA', ".//div[@class='flex p-0'][1]//div[@class='text-style-s-medium text-primary text-primary']")
+            loader.add_xpath('sideB', ".//div[@class='flex p-0'][2]//div[@class='text-style-s-medium text-primary text-primary']")
+            loader.add_xpath('spread', './/div[@class="flex p-0"][1]//button[@data-dd-action-name="Add Bet Selections"][1]/span[1]')
+            loader.add_xpath('spreadA', './/div[@class="flex p-0"][1]//button[@data-dd-action-name="Add Bet Selections"][1]/span[2]')
+            loader.add_xpath('spreadB', './/div[@class="flex p-0"][2]//button[@data-dd-action-name="Add Bet Selections"][1]/span[2]')
+            loader.add_xpath('total', './/div[@class="flex p-0"][1]//button[@data-dd-action-name="Add Bet Selections"][2]/span[1]')
+            loader.add_xpath('over', './/div[@class="flex p-0"][1]//button[@data-dd-action-name="Add Bet Selections"][2]/span[2]')
+            loader.add_xpath('under', './/div[@class="flex p-0"][2]//button[@data-dd-action-name="Add Bet Selections"][2]/span[2]')
+            loader.add_xpath('moneyA', './/div[@class="flex p-0"][1]//button[@data-dd-action-name="Add Bet Selections"][3]/span[2]')
+            loader.add_xpath('moneyB', './/div[@class="flex p-0"][2]//button[@data-dd-action-name="Add Bet Selections"][3]/span[2]')
 
-            # # elements = container.css('button.relative.rounded.flex.h-[48px].flex-1.flex-col.items-center.justify-center.p-1.5.text-style-xs-medium.button-bet-selector-deselected').getall()
-            # elements = container.css('[data-dd-action-name="Add Bet Selections"]')
-            # # # yield {'test': [elem.get() for elem in elements]}
-            # # yield {'test': elements}
+            yield loader.load_item()
 
-            # # Target elements based on class names (without invalid parts)
-            # # elements = response.css('button.relative.rounded.flex.h-[48px].flex-1.flex-col.items-center.justify-center.p-1.5.text-style-xs-medium.button-bet-selector-deselected')
-        
-            # # for element in elements:
-            # #     # Extract text or other attributes as needed
-            # #     text = element.css('::text').getall()
-            # #     yield {
-            # #         'text': text
-            # #     }
-
-            # info = [element.css('::text').getall() for element in elements]
-
-
-            # yield {
-            #     'sideA': container.css('div.text-style-s-medium.text-primary.text-primary::text()').getall()[0],
-            #     'sideB': container.css('div.text-style-s-medium.text-primary.text-primary::text()').getall()[1],
-            #     'spread': info[0][1:],
-            #     'spreadA': info[1],
-            #     'spreadB': info[6],
-            #     'total': info[2].split(' ')[-1],
-            #     'over': info[3],
-            #     'under': info[8],
-            #     'moneyA': info[4],
-            #     'moneyB': info[9],
-            # }
-
-            # sideA = container.xpath('.//div[contains(@class, "text-style-s-medium") and contains(@class, "text-primary")][1]//text()').get()
-            # sideB = container.xpath('.//div[contains(@class, "text-style-s-medium") and contains(@class, "text-primary")][2]//text()').get()
-
-            # sideA = container.css('div.text-style-s-medium.text-primary.text-primary::text()').getall()[0]
-            # sideB = container.css('div.text-style-s-medium.text-primary.text-primary::text()').getall()[1]
-
-            # Extract elements with a specific attribute
-            info = container.xpath('.//button[@data-dd-action-name="Add Bet Selections"]')
-            # info = container.xpath('.//button[@data-dd-action-name="Add Bet Selections"]').getall()
-            # info = container.xpath('.//button[@data-dd-action-name="Add Bet Selections"]//text()').getall()
-
-            yield {
-                'sideA': container.css('div.text-style-s-medium.text-primary.text-primary::text').getall()[0],
-                'sideB': container.css('div.text-style-s-medium.text-primary.text-primary::text').getall()[1],
-                'spread': info[0].css('span::text').getall()[0][1:],
-                'spreadA': info[0].css('span::text').getall()[1],
-                'spreadB': info[3].css('span::text').getall()[1],
-                'total': info[1].css('span::text').getall()[0].split(' ')[-1],
-                'over': info[1].css('span::text').getall()[1],
-                'under': info[4].css('span::text').getall()[1],
-                'moneyA': info[2].css('span::text').getall()[0],
-                'moneyB': info[5].css('span::text').getall()[0],
-            }
-
-            
-            # yield {
-            #     'sideA': container.css('div.text-style-s-medium.text-primary.text-primary::text').getall()[0],
-            #     'sideB': container.css('div.text-style-s-medium.text-primary.text-primary::text').getall()[1],
-            #     'spread': info[0][1:],
-            #     'spreadA': info[1],
-            #     'spreadB': info[6],
-            #     'total': info[2].split(' ')[-1],
-            #     'over': info[3],
-            #     'under': info[8],
-            #     'moneyA': info[4],
-            #     'moneyB': info[9],
-            # }
-
-        # # Target specific container
-        # containers = response.xpath('//div[@class="flex flex-col gap-3 rounded p-4 bg-card-primary-temp"]')
-        
-        # for container in containers:
-        #     # Extract all text within the current container
-        #     all_text = container.xpath('.//text()').getall()
-            
-        #     # Clean up the extracted text
-        #     cleaned_text = ' '.join([text.strip() for text in all_text if text.strip()])
-            
-        #     # Extract text using corrected CSS selector
-        #     elements = container.css('div.text-style-s-medium.text-primary.text-primary span::text').get()
-        #     yield {'container_text': cleaned_text, 'elements_text': elements}
                 
